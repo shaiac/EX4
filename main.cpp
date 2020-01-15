8d3d6c5ecb5617578459b158eb2f878f2b6b24eb
shai acoca <shai.acoca@gmail.com>
2020-01-15 19:52:59 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "StringReverser.h"
 #include "MyClientHandler.h"
 #include "SerialServer.h"
-
+#include "Matrix.h"
 int main() {
-    MyClientHandler *mch = new MyClientHandler();
-    MySerialServer *mss = new MySerialServer();
-    mss->open(7767, mch);
+    //MyClientHandler *mch = new MyClientHandler();
+    //MySerialServer *mss = new MySerialServer();
+    //mss->open(7767, mch);
+
 
     return 0;
 }
